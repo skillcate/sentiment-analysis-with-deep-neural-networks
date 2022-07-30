d5396ff38a11f40ab3430fdecc982fdf8f726d21
SKILLCATE <98591147+skillcate@users.noreply.github.com>
2022-07-30 16:22:39 +0530

    flask deployment underway
    currently facing type error

---
+++ app.py
@@ -0,0 +1,73 @@
+# Library imports
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import re
+from numpy import array
+
+from keras.preprocessing.text import one_hot, Tokenizer
+from keras.models import Sequential, load_model
+from keras.layers import LSTM
+from keras.layers.core import Activation, Dropout, Dense
+from keras.layers import Flatten, GlobalMaxPooling1D, Embedding, Conv1D, LSTM
+from sklearn.model_selection import train_test_split
+from flask import Flask, request, jsonify, render_template
+from keras_preprocessing.sequence import pad_sequences
+import nltk
+from nltk.corpus import stopwords
+from keras_preprocessing.text import tokenizer_from_json
+import io
+import json
+
+stopwords_list = set(stopwords.words('english'))
+maxlen = 100
+
+# Load model
+model_path ='c1_lstm_model_acc_0.864.h5'
+pretrained_lstm_model = load_model(model_path)
+
+# Loading
+with open('b3_tokenizer.json') as f:
+    data = json.load(f)
+    loaded_tokenizer = tokenizer_from_json(data)
+
+
+# Create the app object
+app = Flask(__name__)
+
+
+# creating function for data cleaning
+from b2_preprocessing_function import CustomPreprocess
+custom = CustomPreprocess()
+
+
+# Define predict function
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    query_asis = [str(x) for x in request.form.values()]
+    
+    # Preprocess review text with earlier defined preprocess_text function
+    query_processed = custom.preprocess_text(query_asis)
+    
+    # Tokenising instance with earlier trained tokeniser
+    query_tokenized = loaded_tokenizer.texts_to_sequences(query_processed)
+    
+    # Pooling instance to have maxlength of 100 tokens
+    query_padded = pad_sequences(query_tokenized, padding='post', maxlen=maxlen)
+    
+    # Passing tokenised instance to the LSTM model for predictions
+    query_sentiments = pretrained_lstm_model.predict(query_padded)
+    
+
+    if query_sentiments[0][0]>0:
+        return render_template('index.html', prediction_text=f"Positive Review with probable IMDb rating as: {np.round(unseen_sentiments[0][0]*10,1)}")
+    else:
+        return render_template('index.html', prediction_text=f"Negative Review with probable IMDb rating as: {np.round(unseen_sentiments[0][0]*10,1)}")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ b2_preprocessing_function.py
@@ -0,0 +1,43 @@
+import re
+import nltk
+from nltk.corpus import stopwords
+stopwords_list = set(stopwords.words('english'))
+
+
+TAG_RE = re.compile(r'<[^>]+>')
+
+def remove_tags(text):
+    '''Removes HTML tags: replaces anything between opening and closing <> with empty space'''
+
+    return TAG_RE.sub('', text)
+    
+
+class CustomPreprocess():
+    '''Cleans text data up, leaving only 2 or more char long non-stepwords composed of A-Z & a-z only
+    in lowercase'''
+
+    def __init__(self):
+        pass
+
+    def preprocess_text(self,sen):
+
+        # Remove html tags
+        sentence = remove_tags(sen)
+
+        # Remove punctuations and numbers
+        sentence = re.sub('[^a-zA-Z]', ' ', sentence)
+        
+        # Single character removal
+        sentence = re.sub(r"\s+[a-zA-Z]\s+", ' ', sentence)  # When we remove apostrophe from the word "Mark's", the apostrophe is replaced by an empty space. Hence, we are left with single character "s" that we are removing here.
+
+        # Remove multiple spaces
+        sentence = re.sub(r'\s+', ' ', sentence)  # Next, we remove all the single characters and replace it by a space which creates multiple spaces in our text. Finally, we remove the multiple spaces from our text as well.
+
+        # sentence = sen.lower()
+        sentence = [[word.lower() for word in line.split()] for line in sentence]
+        
+        # Remove Stopwords
+        pattern = re.compile(r'\b(' + r'|'.join(stopwords_list) + r')\b\s*')
+        sentence = pattern.sub('', sentence)
+        
+        return sentence
